6abacf3eea4e10e61339a56c3679be7c39ed436a
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-09-21 14:19:56 -0500

    fetch event listener

--- public/service-workers.js
+++ public/service-workers.js
@@ -53,3 +53,34 @@ self.addEventListener("activate", function (event) {
 );
     self.clients.claim();
 });
+
+// fetch event listener
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then((cache) => {
+                return fetch(evt.request)
+                .then((response) => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch((error) => {
+                    return cache.match(event.request);
+                });
+            })
+            .catch((error) => console.log(error))
+        );
+        return;
+    }
+    event.respondWith(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.match(event.request).then((response) => {
+                return response || fetch(event.request);
+            });
+        })
+    );
+});
