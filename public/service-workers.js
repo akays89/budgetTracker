a470ace0fe9ab329373be527e49d27bd26776b59
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-09-21 12:59:16 -0500

    Install event listener

--- public/service-workers.js
+++ public/service-workers.js
@@ -9,3 +9,30 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
     "styles.css"
 ];
+
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+// installation event listener
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches
+        .open(CACHE_NAME)
+        .then((cache) => {
+            console.log("Your files were pre-cached successfully!");
+            cache
+                .addAll(FILES_TO_CACHE)
+                .then((result) => {
+                    console.log("result of add all", result);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    );
+    self.skipWaiting();
+});
